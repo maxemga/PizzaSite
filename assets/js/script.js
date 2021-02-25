bc51a675ff68272133d69e2e41b013b66d8401f8
maxemga <maxemga22@gmail.com>
2021-02-25 20:09:29 +0300

    almost finished the first page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,22 @@ const galleryContainerText = document.querySelectorAll(".galleryContainerText");
 const headerOverlay = document.getElementById("headerOverlay");
 const galleryIcon1 = document.querySelectorAll(".galleryIcon1")
 const galleryIcon2 = document.querySelectorAll(".galleryIcon2");
+const scrollToHeader = document.querySelector(".scrollToHeader");
+const header = document.querySelector(".header");
+
+const slideImg = document.querySelectorAll(".slideContentImg")
+const slideTitle = document.querySelectorAll(".slideContentTitle")
+const slideText = document.querySelectorAll(".slideContentText")
+const slideButton1 = document.querySelectorAll(".slideContentButton1")
+const slideButton2 = document.querySelectorAll(".slideContentButton2")
+const slideClickEvent = document.querySelector(".slideClickEvent");
 
 
 
 burger.addEventListener("click", OpenNavModal);
 navModalBlack.addEventListener("click", CloseModalBlock);
+
+scrollToHeader.addEventListener("click", ScrollToHeader);
 
 
 [].forEach.call(galleryContainer,function(el){
@@ -19,7 +30,15 @@ navModalBlack.addEventListener("click", CloseModalBlock);
     el.addEventListener("mouseout", RemoveAnimationGallery);
 });
 
-
+window.addEventListener("load", AnimationSlider);
+window.addEventListener("scroll", function() {
+    if(window.scrollY >= 1000) {
+        scrollToHeader.classList.add("activeScrollHeader");
+    }
+    if(window.scrollY <= 1000) {
+        scrollToHeader.classList.remove("activeScrollHeader");
+    }
+})
 window.addEventListener("scroll", function() {
     if(window.scrollY >= 10) {
         headerOverlay.classList.add("activityHeader")
@@ -27,20 +46,50 @@ window.addEventListener("scroll", function() {
     else {
         headerOverlay.classList.remove("activityHeader")
     }
+
+    
 })
 
+function ScrollToHeader() {
+    header.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    })
+}
 
-function AddAnimationGallery() {
+function AnimationSlider() {
+    setTimeout(() => {
+        CycleForSlider(slideTitle);
+        setTimeout(() => {
+            CycleForSlider(slideImg)
+            setTimeout(() => {
+                CycleForSlider(slideText)
+                setTimeout(() => {
+                    CycleForSlider(slideButton1)
+                }, 300);
+                setTimeout(() => {
+                    CycleForSlider(slideButton2);
+                }, 500);
+            }, 300);
+        }, 300);
+    }, 800);
+}
+
+function CycleForSlider(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].classList.add("activeSlider");      
+    }  
+}
+
+function AddAnimationGallery(event) {
     for( let i = 0; i < galleryContainer.length; i++){ 
         galleryContainerImage[i].classList.add("activeGallery");
         galleryContainerText[i].classList.add("activeGalleryText");
         galleryIcon1[i].classList.add("activityIcon");
         galleryIcon2[i].classList.add("activityIcon");
-	}
-    
-    
-    
+	}  
 }
+
 function RemoveAnimationGallery() {
     for( let i = 0; i < galleryContainer.length; i++){ 
         galleryContainerImage[i].classList.remove("activeGallery");
@@ -58,49 +107,4 @@ function CloseModalBlock() {
     navModal.classList.remove("activeNavModal");
     navModalBlack.classList.remove("activeNavBlack");
 }
-
-
-// const burger = document.getElementById("burger");
-// const navModal = document.getElementById("modalNavBlock")
-// const navModalBlack = document.getElementById("modalNavBlack");
-// const galleryContainer = document.getElementById("galleryContainer")
-// const galleryContainerImage = document.getElementById("galleryContainerImage");
-// const galleryContainerText = document.getElementById("galleryContainerText");
-// const headerOverlay = document.getElementById("headerOverlay");
-
-
-
-// burger.addEventListener("click", OpenNavModal);
-// navModalBlack.addEventListener("click", CloseModalBlock);
-
-// galleryContainer.addEventListener("mouseover", AddAnimationGallery);
-// galleryContainer.addEventListener("mouseout", RemoveAnimationGallery);
-
-// window.addEventListener("scroll", function() {
-//     if(window.scrollY >= 10) {
-//         headerOverlay.classList.add("activityHeader")
-//     }
-//     else {
-//         headerOverlay.classList.remove("activityHeader")
-//     }
-// })
-
-
-// function AddAnimationGallery() {
-//     galleryContainerImage.classList.add("activeGallery");
-//     galleryContainerText.classList.add("activeGalleryText");
-// }
-// function RemoveAnimationGallery() {
-//     galleryContainerImage.classList.remove("activeGallery");
-//     galleryContainerText.classList.remove("activeGalleryText");}
-
-// function OpenNavModal() {
-//     navModal.classList.add("activeNavModal");
-//     navModalBlack.classList.add("activeNavBlack");
-// }
-// function CloseModalBlock() {
-//     navModal.classList.remove("activeNavModal");
-//     navModalBlack.classList.remove("activeNavBlack");
-// }
-
 
